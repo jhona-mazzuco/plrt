73d79d8bae0d3667ab82fae02cec7347d316e237
Jhonattan Loli Mazzuco <jhonattan.mazzuco.ext@esferaenergia.com.br>
2023-07-14 23:24:06 -0300

    adiciona logo da PL hospedado

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { Row } from '../interfaces/row.interface';
 import { Trophy } from '../interfaces/trophy.interface';
 import { TROPHY_VALUE } from '../constants/trophy-value.constant';
 import { Team } from '../interfaces/team.interface';
-import teams from '../../assets/trophies.json';
+import teams from '../../assets/data.json';
 
 @Injectable({
   providedIn: 'root'
